f92f62d58aee4d100fd62645e8d440e50b7d6cbb
Rodrigo Alencar <rcalencar@gmail.com>
2018-02-08 10:18:21 -0500

    no message

---
+++ app/src/main/java/com/example/rodrigo/guessinggame/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.rodrigo.guessinggame
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        if (savedInstanceState == null) {
+            Log.d(LOG_TAG, "onCreate adding fragment")
+            // During initial setup, plug in the fragment.
+            val fragment = MainActivityFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_gameboard, fragment, "fragment_gameboard").commit()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+
+    companion object {
+        private val LOG_TAG = MainActivity::class.java.getSimpleName()
+    }
+}
